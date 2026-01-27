2362bcca2ba6aa95790db6f639b820f51631fdc0
Adil Hussain <silver@Adils-MacBook-Air.local>
2026-01-27 17:55:43 +0530

    business category auth removed

--- src/modules/admin/business_category/admin.business_categories.routes.js
+++ src/modules/admin/business_category/admin.business_categories.routes.js
@@ -1,23 +1,51 @@
-import express from "express";  
-import { getBusinessCategories, getAllBusinessCategoryById, createBusinessCategory, updateBusinessCategory, deleteBusinessCategory, updateBusinessCategoryStatus } from "./admin.business_categories.controller.js";
-import { authenticate } from "../../../shared/middlewares/auth.middleware.js";  
-import { authorize } from "../../../shared/middlewares/role.middleware.js";  
-import { ROLES } from "../../../shared/constants/roles.js";  
-import { validate } from "../../../shared/middlewares/validate.middleware.js";  
+import express from "express";
 import {
-  createBusinessCategorySchema,
-} from "./admin.business_categories.schema.js";
-
+  getBusinessCategories,
+  getAllBusinessCategoryById,
+  createBusinessCategory,
+  updateBusinessCategory,
+  deleteBusinessCategory,
+  updateBusinessCategoryStatus,
+} from "./admin.business_categories.controller.js";
+import { authenticate } from "../../../shared/middlewares/auth.middleware.js";
+import { authorize } from "../../../shared/middlewares/role.middleware.js";
+import { ROLES } from "../../../shared/constants/roles.js";
+import { validate } from "../../../shared/middlewares/validate.middleware.js";
+import { createBusinessCategorySchema } from "./admin.business_categories.schema.js";
 
 const router = express.Router();
 
-router.get("/", authenticate, authorize(ROLES.ADMIN), getBusinessCategories);  
-router.get("/:id", authenticate, authorize(ROLES.ADMIN), getAllBusinessCategoryById);
-router.post("/", authenticate, authorize(ROLES.ADMIN), validate(createBusinessCategorySchema), createBusinessCategory);
-router.put("/:id", authenticate, authorize(ROLES.ADMIN), updateBusinessCategory);
-router.patch("/:id/status", authenticate, authorize(ROLES.ADMIN), updateBusinessCategoryStatus);
-router.delete("/:id", authenticate, authorize(ROLES.ADMIN), deleteBusinessCategory);
-
-
+router.get("/", getBusinessCategories);
+router.get(
+  "/:id",
+  authenticate,
+  authorize(ROLES.ADMIN),
+  getAllBusinessCategoryById,
+);
+router.post(
+  "/",
+  authenticate,
+  authorize(ROLES.ADMIN),
+  validate(createBusinessCategorySchema),
+  createBusinessCategory,
+);
+router.put(
+  "/:id",
+  authenticate,
+  authorize(ROLES.ADMIN),
+  updateBusinessCategory,
+);
+router.patch(
+  "/:id/status",
+  authenticate,
+  authorize(ROLES.ADMIN),
+  updateBusinessCategoryStatus,
+);
+router.delete(
+  "/:id",
+  authenticate,
+  authorize(ROLES.ADMIN),
+  deleteBusinessCategory,
+);
 
 export default router;
